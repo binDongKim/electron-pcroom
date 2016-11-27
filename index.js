52a3a7e2f81a2d97f0ab552f9be85b1079678fd4
Dongbin Kim <bindongkim@gmail.com>
2016-11-27 23:10:31 +0900

    DB에서 받아온 데이터의 갯수에 따라 동적으로 버튼을 그려줌

--- index.js
+++ index.js
@@ -20,17 +20,41 @@ connection.query('SELECT * FROM ElectronTestTable', function(err, results) {
         console.log(err);
         return;
     }
-    //document.getElementsByTagName("h1")[0].innerHTML = rows[0].id;
     console.log("SELECT QUERY SUCCEEDED");
+
+    // 받아온 results array로 div button 그려주자
+    // Row 0: 0123 4567
+    // Row 1: 891011 12131415
+    // col-xs-6 div의 인덱스는 각 버튼의 인덱스를 4로나눈 몫과 같다.
+
+    var containerDIV = document.getElementsByClassName('container');
+
+    for (var i = 0; i < results.length; i++) {
+        if(i%8 == 0) {
+            var rowDIV = document.createElement('div');
+            rowDIV.setAttribute('class', 'row');
+            containerDIV[0].appendChild(rowDIV);
+        }
+        if(i%4 == 0) {
+            var halfColDIV = document.createElement('div');
+            halfColDIV.setAttribute('class', 'col-xs-6');
+            document.getElementsByClassName('row')[Math.floor(i/8)].appendChild(halfColDIV);
+        }
+        var button = document.createElement('button');
+        button.setAttribute('class', 'btn btn-default');
+        button.setAttribute('type', 'button');
+        // button.innerHTML = results[i];
+        button.innerHTML = "좌석 " + (i+1);
+        var whichGrid = Math.floor(i / 4) % 2;
+        document.getElementsByClassName('col-xs-6')[Math.floor(i/4)].appendChild(button);
+    }
 });
+
 
 function updateTheSeat() {
     var theTag = event.srcElement;
     var seatID = theTag.dataset.seatid;
     var occupied = theTag.dataset.occupied;
-    console.log(theTag);
-    console.log(seatID);
-    console.log(occupied);
 
     $query = 'UPDATE ElectronTestTable SET occupied = ' + connection.escape(occupied == 0 ? 1 : 0) + ' WHERE id = ' + connection.escape(seatID);
     connection.query($query, function(err, results) {
