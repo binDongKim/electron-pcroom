31813d3fc5dc801aec2318aa5dba8dd8fe17e601
Dongbin Kim <bindongkim@gmail.com>
2016-12-02 13:15:20 +0900

    ajax비동기 요청으로 이미 구현되어있는 php url에 post요청을 보내서 특정pc방의 - 좌석정보 받아오기 - 클릭이벤트를 통해 좌석 empty(occupied)여부 업데이트

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*
+  특정 pc방(ex. 제논)의 좌석리스트를 받아오는 부분
+*/
 var req = new XMLHttpRequest();
 var url = "http://pcbanghelp.dothome.co.kr/seat.php";
 var params = "name=제논";
@@ -7,7 +10,64 @@ req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
 req.onreadystatechange = function() {
     if(req.readyState == XMLHttpRequest.DONE && req.status == 200) {
-        console.log(req.response);
+        var data = JSON.parse(req.response);
+        console.log(data);
+        // 받아온 data에서 사용할 property는 empty, id(seatid)
+
+        // 받아온 data(Object) array로 div button 그려주자
+        // Row 0: 0123 4567
+        // Row 1: 891011 12131415
+        // col-xs-6 div의 인덱스는 각 버튼의 인덱스를 4로나눈 몫과 같다.
+
+        var containerDIV = document.getElementsByClassName('container');
+
+        for (var i = 0; i < data.length; i++) {
+            if(i%8 == 0) {
+                var rowDIV = document.createElement('div');
+                rowDIV.setAttribute('class', 'row');
+                containerDIV[0].appendChild(rowDIV);
+            }
+            if(i%4 == 0) {
+                var halfColDIV = document.createElement('div');
+                halfColDIV.setAttribute('class', 'col-xs-6');
+                document.getElementsByClassName('row')[Math.floor(i/8)].appendChild(halfColDIV);
+            }
+            var button = document.createElement('button');
+            button.setAttribute('type', 'button');
+            button.setAttribute('class', data[i]['empty'] == 0 ? 'btn btn-default btn-lg' : 'btn btn-primary btn-lg');
+            button.setAttribute('data-seatid', data[i]['id']);
+            button.setAttribute('data-occupied', data[i]['empty']);
+            button.addEventListener('click', updateTheSeat);
+            button.innerHTML = "좌석 " + (i+1);
+            var whichGrid = Math.floor(i / 4) % 2;
+            document.getElementsByClassName('col-xs-6')[Math.floor(i/4)].appendChild(button);
+        }
     }
 }
 req.send(params);
+
+/*
+  해당 좌석의 empty여부가 클릭할때마다 수정되는 부분
+*/
+function updateTheSeat() {
+    var theTag = event.srcElement;
+    var seatID = theTag.dataset.seatid;
+    var occupied = theTag.dataset.occupied;
+
+    var req = new XMLHttpRequest();
+    var url = "http://pcbanghelp.dothome.co.kr/seatUpdate.php";
+    var params = "name=제논&id=" + seatID + "&empty=" + (occupied == 0 ? 1 : 0);
+    req.open("POST", url, true);
+
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    req.onreadystatechange = function() {
+      if(req.readyState == XMLHttpRequest.DONE && req.status == 200) {
+          if(req.response == 1) {
+              theTag.setAttribute('data-occupied', occupied == 0 ? 1 : 0);
+              theTag.setAttribute('class', theTag.getAttribute('data-occupied') == 0 ? 'btn btn-default btn-lg' : 'btn btn-primary btn-lg');
+          }
+      }
+    }
+    req.send(params);
+}
